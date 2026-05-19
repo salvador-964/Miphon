f8c6df0c6700269ecfcca0ec8330d94581af3800
salvador-964 <sse0484@gmail.com>
2026-05-19 20:44:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,8 +10,8 @@ const ASSETS = [
   './styles.css',
   './app.js',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 // Instalar: pre-cachear todos los recursos estáticos
